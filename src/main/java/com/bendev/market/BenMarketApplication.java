1fa5d581a9875ddb664ebd900759ee836d9f5239
BSbenjaminsoto <benjaminsoto661@hotmail.com>
2024-08-28 23:55:10 -0400

    firts commit

---
+++ src/main/java/com/bendev/market/BenMarketApplication.java
@@ -0,0 +1,13 @@
+package com.bendev.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bendev/market/BenMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bendev.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
